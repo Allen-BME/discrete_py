75c77c7ada219de40afaed5c878bbcf8a4f95e63
Allen-BME <carter_allen@student.uml.edu>
2023-03-27 09:31:39 -0400

    get identity element

--- discrete.py
+++ discrete.py
@@ -1,3 +1,4 @@
+import itertools
 import types
 import numpy as np
 
@@ -16,32 +17,55 @@ class AlgebraicSystem:
         self.__op = np.array(operations)
         self.__num_op = self.__op.shape[0] - 1
         self.__u = np.unique(universe)
+        self.__check_closure()
         self.__associative = None
+        self.__e = None
+        self.__checked_e = False
 
     
     def is_associative(self):
         """
-        return 1 if associative, else 0
-        TODO: address memory complexity shape=(n, n, n, 3) not (n, 3)?
+        return True if associative, else False.
+        WARNING: takes really really long as |U| increases
         """
         if self.__associative is not None:
             return self.__associative
-        product = cartesian_product(
-                self.__u, self.__u, self.__u)
-        for (a,b,c) in product:
+        for (a,b,c) in itertools.product(
+                self.__u, self.__u, self.__u):
             if self.operation(
                     self.operation(a,b), c) != self.operation(
                             a, self.operation(b,c)):
-                self.__associative = 0
-                return 0
-        self.__associative = 1
-        return 1
+                self.__associative = False
+                return False
+        self.__associative = True
+        return True
 
+
+    def get_identity(self, op_idx=0):
+        """
+        return identity element if there is one, else None
+        """
+        if (self.__checked_e) and (self.__e is None):
+            return self.__e
+        self.__checked_e = True
+        for e in self.__u:
+            e_is_identity = True
+            for a in self.__u:
+                if ((self.__op[op_idx](a, e) != self.__op[op_idx](e, a)) or
+                        self.__op[op_idx](a, e) != a):
+                    e_is_identity = False
+                    break
+            if e_is_identity:
+                self.__e = e
+                return e
+                    
 
     def operation(self, lop, rop, idx=0):
         """
         perform binary operation as lop * rop
         """
+        self.__check_op(lop)
+        self.__check_op(rop)
         if (lop not in self.__u) or (rop not in self.__u):
             raise Exception("invalid operation: operand(s) not in universe")
         self.__check_op_idx(idx)
@@ -63,9 +87,48 @@ class AlgebraicSystem:
                 fp.write(row + "\n")
 
 
+    def __check_closure(self):
+        for op_idx in range(self.__num_op):
+            for (a,b) in itertools.product(self.__u, self.__u):
+                if self.operation(a, b, op_idx=i) not in self.__u:
+                    raise Exception(f"system")
+
+
     def __check_op_idx(self, idx):
         if idx > self.__num_op:
             raise Exception(f"idx={idx} outside of operation array bounds")
+
+
+    def __check_op(self, op):
+        if op not in self.__u:
+            raise Exception(
+                    f"invalid operation: operand '{op}' not in universe")
+
+
+
+class Z_n:
+
+    """
+    Class implementation of Group Z_n equipped with operation plus_mod_n.
+    """
+
+    def __init__(self, n):
+        self.n = n
+        self.__u = np.arange(n)
+
+
+    def plus_mod_n(self, lop, rop):
+        self.__check_op(lop)
+        self.__check_op(rop)
+        return (lop + rop) % self.n
+
+
+    def is_associative(self):
+        return True
+
+
+    def get_identity(self):
+        return 0
 
 
     def __check_op(self, op):
--- main.py
+++ main.py
@@ -2,7 +2,36 @@ import time
 import numpy as np
 import discrete
 
-n = 100
+"""
+NOTE: it's not the get identity that's taking forever,
+it's creating the U array!
+Also, when N gets too large, the size of the array is the limit,
+not the speed of getting identity
+"""
+
+N = 10000
+
+def op(a: str, b: str):
+    return a + b
+
+u = ["bah"*(N-1-i) for i in range(N)]
+
+system = discrete.AlgebraicSystem(u, [op])
+
+print("system made")
+
+t1 = time.time()
+print(system.get_identity())
+t2 = time.time()
+print(t2-t1)
+
+t1 = time.time()
+print(system.get_identity())
+t2 = time.time()
+print(t2-t1)
+
+"""
+n = 100_000
 
 Z_n = range(n)
 
@@ -13,12 +42,12 @@ def plus_mod_n(lop: int, rop: int):
 system = discrete.AlgebraicSystem(Z_n, [plus_mod_n])
 
 t1 = time.time()
-print(system.is_associative())
+print(system.get_identity())
 t2 = time.time()
 print(t2-t1)
 
 t1 = time.time()
-print(system.is_associative())
+print(system.get_identity())
 t2 = time.time()
 print(t2-t1)
-
+"""
